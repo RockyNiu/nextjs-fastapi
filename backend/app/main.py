b7ef75063096765b9dfb5b4e57a049bb9de51185
Lei Niu <lei.niu.ny@gmail.com>
2024-04-20 23:35:19 -0400

    Add backend docker

---
+++ backend/app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+import random
+
+app = FastAPI()
+
+# Configure CORS
+origins = [
+  "http://localhost",
+  "http://localhost:3000",
+  # Add more allowed origins if needed
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+RootRouter = APIRouter()
+@RootRouter.get("/")
+def read_root():
+  return "Hellow World!"
+
+@RootRouter.get("/random")
+def get_random():
+  items = ["item1", "item2", "item3", "item4"]  # Replace with your list of items
+  random_item = random.choice(items)
+  return random_item
+
+app.include_router(RootRouter)
+
