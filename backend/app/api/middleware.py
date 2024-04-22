9a65bfc415719d5a5f161ed19b77b79804722dc0
Lei Niu <lei.niu.ny@gmail.com>
2024-04-22 11:58:02 -0400

    Add BaseMiddleare in api

---
+++ backend/app/api/middleware.py
@@ -0,0 +1,14 @@
+from backend.app.database import database_context
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class BaseMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        with database_context():
+            try:
+                response = await call_next(request)
+            except Exception as e:
+                raise e
+
+        return response
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,34 +1,40 @@
-from fastapi import FastAPI, APIRouter
-from fastapi.middleware.cors import CORSMiddleware
 import random
+
+from backend.app.api.middleware import BaseMiddleware
+from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Configure CORS
 origins = [
-  "http://localhost",
-  "http://localhost:3000",
-  # Add more allowed origins if needed
+    "http://localhost",
+    "http://localhost:3000",
+    # Add more allowed origins if needed
 ]
 
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=origins,
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+app.add_middleware(BaseMiddleware)
 
 RootRouter = APIRouter()
+
+
 @RootRouter.get("/")
 def read_root():
-  return "Hellow World!"
+    return "Hellow World!"
+
 
 @RootRouter.get("/random")
 def get_random():
-  items = ["item1", "item2", "item3", "item4"]  # Replace with your list of items
-  random_item = random.choice(items)
-  return random_item
+    items = ["item1", "item2", "item3", "item4"]  # Replace with your list of items
+    random_item = random.choice(items)
+    return random_item
+
 
 app.include_router(RootRouter)
-
